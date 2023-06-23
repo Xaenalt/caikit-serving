ab7b70244270de44496587ace2051499a30d795d
Sean Pryor <spryor@redhat.com>
2023-06-22 22:50:32 -0400

    Variable-ize start-serving.sh for cleanliness

--- start-serving.sh
+++ start-serving.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sed "s/TGIS_HOSTNAME/${TGIS_HOSTNAME}/" /caikit/config/caikit-tgis.template.yml > /caikit/config/caikit-tgis.yml
-export CONFIG_FILES=/caikit/config/caikit-tgis.yml
+TGIS_CONFIG_TEMPLATE='/caikit/config/caikit-tgis.template.yml'
+TGIS_CONFIG_FILE='/caikit/config/caikit-tgis.yml'
+
+sed "s/TGIS_HOSTNAME/${TGIS_HOSTNAME}/" "${TGIS_CONFIG_TEMPLATE}" > "${TGIS_CONFIG_FILE}"
+export CONFIG_FILES="${TGIS_CONFIG_FILE}
 
 exec python3 -m caikit.runtime.grpc_server
